006f7d311e2cbc53cac9e65808ce046867bf44f9
Anastasia Voronkina <voronkina-nastena@mail.ru>
2023-10-27 21:07:21 +0300

    Сделан адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,15 @@ const popup = document.querySelector('.popup');
 
 function openPopup() {
    popup.classList.add('popup_open')
+   document.body.classList.add('lock');
 }
 
 function closePopup(event) {
-   if (!event.target.closest('.popup__content'))
-      popup.classList.remove('popup_open')
+   if (!event.target.closest('.popup__content')) {
+      popup.classList.remove('popup_open');
+      document.body.classList.remove('lock');
+   }
+
 }
 
 
